cf2ecbbcf128eac481409662d00d793e21e22fe4
Gabriel Sousa <gabriel.mywork@proton.me>
2025-05-22 09:10:30 -0300

    fix bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div className="flex">
-        <aside className="w-64">
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950">
           sidebar
         </aside>
-        <main className="flex-1">main</main>
+        <main className="flex-1">
+          main
+        </main>
       </div>
-      <footer>footer</footer>
+      <footer>
+        footer
+      </footer>
     </div>
   );
 }
